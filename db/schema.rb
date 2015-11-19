a798c0f8c0e0b1002486404dcb7def70ff2d91eb
Chris Willis <willis.chris.b@gmail.com>
2015-11-19 12:01:43 -0500

    third push

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119134339) do
+ActiveRecord::Schema.define(version: 20151119145119) do
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "comment"
+    t.integer  "holo_score"
+    t.integer  "business_id"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "reviews", ["business_id"], name: "index_reviews_on_business_id"
+  add_index "reviews", ["user_id"], name: "index_reviews_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
